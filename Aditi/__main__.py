a66f225f3a3fdfe93ccc313fe2a4ae374a5699ab
Shubhxspam <164321506+Shubhxspam@users.noreply.github.com>
2024-05-20 20:17:23 +0530

    Update __main__.py

--- Aditi/__main__.py
+++ Aditi/__main__.py
@@ -17,7 +17,7 @@ async def anony_boot():
     for all_module in ALL_MODULES:
         importlib.import_module("Aditi.modules." + all_module)
 
-    LOGGER.info(f"@{SnehaBot.username} Started.")
+    LOGGER.info(f"@snehaxprobot Started.")
     await idle()
 
 
